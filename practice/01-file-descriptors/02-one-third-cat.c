812e7228a04503d627ce48bbe1e98387ae2a622e
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-11-05 15:25:15 +0200

    feat(01-file-descriptors): cat + 1/3 cat implementation
    Added tasks completed during the 05.11.2024 lecture.

--- practice/01-file-descriptors/02-one-third-cat.c
+++ practice/01-file-descriptors/02-one-third-cat.c
@@ -1,38 +1,22 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
-#define BYTES_TO_READ 10
+#define BYTES_TO_READ 1
 
 int main(int argc, char **argv) {
-  // Check if correct amount of args is supplied.
+  // Check if correct amount of args is supplied
   if (argc != 2) {
     fprintf(stderr, "Invalid arguments.");
     return 1;
   }
 
-  // Open the file specified in argv.
+  // Open the file specified in argv
   int fd = open(argv[1], O_RDONLY);
   if (fd == -1) {
     perror("open");
     return 1;
   }
 
-  // Variant 1 for reading from file.
-  /* char read_buf[10]; */
-  /* int bytes_read = read(fd, read_buf, 10); */
-  /* while (bytes_read != 0) { */
-  /*   bytes_read = read(fd, read_buf, 10); */
-  /*   write(STDIN_FILENO, read_buf, 10); */
-  /* } */
-
-  // Variant 2 for reading from file.
-  /* char read_buf[10]; */
-  /* int bytes_read; */
-  /* while ((bytes_read = read(fd, read_buf, 10)) != 0) { */
-  /*   write(STDIN_FILENO, read_buf, 10); */
-  /* } */
-
-  // Variant 3 for reading from file.
   char read_buf[BYTES_TO_READ];
   int bytes_read;
   do {
@@ -44,21 +28,27 @@ int main(int argc, char **argv) {
       return -1;
     }
 
-    // Print the number of bytes read.
-    /* printf("<[%d]>\n", bytes_read); */
-
     // Write to the standard output
     if (write(STDIN_FILENO, read_buf, bytes_read) == -1) {
       perror("write");
     }
+
+    // Skip 2 bytes from the current position.
+    int offset = lseek(fd, 2, SEEK_CUR);
+
+    // Print the current offset from the begining of the file after skipping 2
+    // bytes.
+    /* printf("<[%d]>\n", offset); */
   } while (bytes_read == BYTES_TO_READ);
-  /* } while (bytes_read != 0); */
+
+  // Print the length of the file
+  int offset = lseek(fd, 0, SEEK_END);
+  printf("<[%d]>\n", offset);
 
   // Close file
   if (close(fd) == -1) {
     perror("open");
     return 1;
   }
-
   return 0;
 }
